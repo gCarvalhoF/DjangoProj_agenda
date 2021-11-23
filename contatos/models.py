034f648f62c546d50e0482dfd19082e4bbf42ab2
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2021-11-23 09:11:44 -0300

    Registered the new models Contato & Categoria in Admin (Aula 3 | 3rd Class)

--- contatos/models.py
+++ contatos/models.py
@@ -5,12 +5,19 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
-    data_criacao = models.DateTimeField(default=timezone.now)
+    data_criacao = models.DateTimeField(
+        default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
