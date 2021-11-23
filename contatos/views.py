c9f01e48f2b96aead6d5669ecc1857f69f07d76c
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2021-11-23 10:04:45 -0300

    Added Details page and made it render dinamically data from the given Id (Aula 5 | 5th Class)

--- contatos/views.py
+++ contatos/views.py
@@ -10,3 +10,11 @@ def index(request):
     return render(request, 'contatos/index.html', {
         'contatos': contatos,
     })
+
+
+def details(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+
+    return render(request, 'contatos/detalhes.html', {
+        'contato': contato,
+    })
